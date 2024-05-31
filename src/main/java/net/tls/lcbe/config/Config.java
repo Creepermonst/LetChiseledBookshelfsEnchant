d00581116c77f2c2044c6fcc1193e7feb8b50d50
Creepermonst <adam.harel30@gmail.com>
2024-05-31 18:57:28 +0300

    🔧Fixed Config File to work with ModMenu

--- src/main/java/net/tls/lcbe/config/Config.java
+++ src/main/java/net/tls/lcbe/config/Config.java
@@ -1,6 +1,9 @@
 package net.tls.lcbe.config;
 
+import io.wispforest.owo.config.annotation.Modmenu;
+
 @io.wispforest.owo.config.annotation.Config(name = "lcbe-config", wrapperName = "Config")
+@Modmenu(modId = "lcbe")
 public class Config {
 	public Choices chiseledBookshelfType = Choices.ANY_BOOKSHELF;
 	public int fillCount = 3;
