977aa7310c0c2e27f8c3349007fe0004edf99552
Creepermonst <adam.harel30@gmail.com>
2024-05-31 17:42:53 +0300

    ✨Initial Commit

---
+++ src/main/java/net/tls/lcbe/LCBE.java
@@ -0,0 +1,15 @@
+package net.tls.lcbe;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LCBE implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Let Chiseled Bookshelves Enchant!");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
