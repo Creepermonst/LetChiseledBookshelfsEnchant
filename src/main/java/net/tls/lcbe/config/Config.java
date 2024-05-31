0c2d83e58f1b8564ae50018ee32476a0c4d6f60c
Creepermonst <adam.harel30@gmail.com>
2024-05-31 19:02:15 +0300

    🤡Fixed Config File AGAIN

--- src/main/java/net/tls/lcbe/config/Config.java
+++ src/main/java/net/tls/lcbe/config/Config.java
@@ -1,15 +1,12 @@
 package net.tls.lcbe.config;
 
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.RangeConstraint;
 
 @io.wispforest.owo.config.annotation.Config(name = "lcbe-config", wrapperName = "Config")
 @Modmenu(modId = "lcbe")
 public class Config {
-	public Choices chiseledBookshelfType = Choices.ANY_BOOKSHELF;
+	@RangeConstraint(min = 1, max = 6)
 	public int fillCount = 3;
 	public boolean hasToBeEnchanted = false;
-
-	public enum Choices {
-		ANY_BOOKSHELF, FILLED_BOOKSHELF;
-	}
 }
